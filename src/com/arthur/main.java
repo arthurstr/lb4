032d6041700fa6463ac6fb6366cd60e411daeded
arthurstr <74472382+arthurstr@users.noreply.github.com>
2021-12-16 16:30:48 +0300

    Create class Main

---
+++ src/com/arthur/main.java
@@ -0,0 +1,12 @@
+package com.arthur;
+
+import javax.swing.*;
+
+public class main {
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
